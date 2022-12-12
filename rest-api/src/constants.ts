d7563d8b835a0fe917acbba38e619d49e90c7f36
Asiia Khytrych <45557681+Asiia56@users.noreply.github.com>
2022-12-12 23:21:15 +0200

    Update constants.ts

--- rest-api/src/constants.ts
+++ rest-api/src/constants.ts
@@ -1,3 +1,3 @@
-export const MONGO_CONNECTION = 'mongodb+srv://nestjs-admin:q7zPMl2BaP3AoZRb@cluster0.votmoh9.mongodb.net/nestjs-cranes?retryWrites=true&w=majority';
+export const MONGO_CONNECTION = 'mongodb+srv://nestjs-admin';
 
-export const JWT_SECRET = '0Waspif4akud';
+export const JWT_SECRET = '';
